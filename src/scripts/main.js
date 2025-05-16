158fe6ad56f1d07fb20df55a2e9fb0bd9c0ca4ae
Maciej Szulim <maciejszulim@gmail.com>
2025-05-16 20:45:50 +0200

    trying to solve refresh aos issue

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,15 @@
-import AOS from "aos";
-import "aos/dist/aos.css";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 AOS.init({
-  once: false,
-  delay: 100,
-  easing: "ease-in-out-sine",
-  duration: 400,
+    once: false,
+    delay: 100,
+    easing: 'ease-in-out-sine',
+    duration: 400
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    setTimeout(function () {
+        AOS.refresh();
+    }, 500);
 });
