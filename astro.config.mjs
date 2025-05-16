7683b4cf331f7ff7abedc593853bacec7c6094df
Maciej Szulim <maciejszulim@gmail.com>
2025-05-16 20:18:06 +0200

    import cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
---
+++ src/scripts/main.js
@@ -0,0 +1,8 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init({
+  once: false,
+  delay: 100,
+  easing: "ease-in-out-sine",
+  duration: 400,
+});
